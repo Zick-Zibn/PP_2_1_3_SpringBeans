d4002151708604c27acac5613dffeab24e781b44
Zick_Zibn <karpenko_ilia@list.ru>
2026-01-29 18:46:34 +0300

    Add

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dog extends Animal {
-
+    String name;
     public Dog() {
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
